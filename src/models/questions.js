cbad0ff96f3098a0e7956e92f3a3d3ffce0ee2a7
Kayla Elortondo <kaylaelortondo@Kaylas-Mac-mini.local>
2021-05-06 14:10:30 -0600

    Implemented the ability to post to questions

--- src/models/questions.js
+++ src/models/questions.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const QuestionsSchema = new Schema({});
+const QuestionsSchema = new Schema({}, { strict: false });
 
 QuestionsSchema.index({ product_id: 1 });
 
