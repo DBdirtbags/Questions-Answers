36933a90ae03912e044aead43eff3a22802c13d0
Kayla Elortondo <kaylaelortondo@Kaylas-Mac-mini.local>
2021-05-07 11:36:41 -0600

    Wrote testing for questions endpoint

--- config/server.js
+++ config/server.js
@@ -5,17 +5,7 @@ const app = express();
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-const port = 3001;
-
 const qaRoutes = require('../src/routes.js');
 app.use('/qa', qaRoutes);
 
-app.listen(port).on('error', (err) => {
-  console.log('Error starting server');
-  console.error(err);
-  process.exit(0);
-}).on('listening', () => {
-  console.log('Server successfully started. Listening on port ' + port + '.');
-})
-
-module.exports = { app }
+module.exports = app;
